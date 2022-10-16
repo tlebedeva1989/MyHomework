29e1112688d7817ebfe31dc544f11c491e08ea04
Tlebedeva1989 <tlebedeva1989@gmail.com>
2022-10-16 08:09:16 +0300

    fourth task complete

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -67,5 +67,24 @@ else{
 } */
 
 
+/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8  */
+
+/* Console.WriteLine("Input natural digital");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while(current<n)
+{
+    if (current%2 ==0)
+    {    Console.Write($"{current}, ");
+    }
+    current++;
+}*/
+
+
+
+
 
 
