a46596084843b27c38d48a3e84e8a627a9115a68
Tlebedeva1989 <tlebedeva1989@gmail.com>
2022-10-16 07:29:29 +0300

    first task complete

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,4 +4,15 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
-Console.WriteLine("add first number: ");
+Console.WriteLine ("input the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+   Console.WriteLine($"First Number {firstNumber} is MAX, Second Number {secondNumber} is MIN");
+}
+else 
+{
+   Console.WriteLine($"First Number {firstNumber} is MIN, Second Number {secondNumber} is MAX");
+}
