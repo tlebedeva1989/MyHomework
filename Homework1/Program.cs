483ddb611cbe47e1c85cbcda410b0e23f210c095
Tlebedeva1989 <tlebedeva1989@gmail.com>
2022-10-16 08:00:50 +0300

    third task complete

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -50,6 +50,22 @@ else {
 }
 Console.WriteLine($"Max number is {maxim}"); */
 
+/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет */
+
+/* Console.WriteLine("input natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number %2 == 0)
+{
+    Console.WriteLine($"Number {number} is even number.");
+}
+else{
+    Console.WriteLine($"Number {number} is uneven number.");
+} */
+
 
 
 
