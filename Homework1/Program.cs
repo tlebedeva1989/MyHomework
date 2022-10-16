7c0aa58eff9bc1b91700b92af306dead74a70019
Tlebedeva1989 <tlebedeva1989@gmail.com>
2022-10-16 07:53:05 +0300

    second task complete

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,7 +4,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
-Console.WriteLine ("input the first number: ");
+/*Console.WriteLine ("input the first number: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("input the second number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
@@ -15,4 +15,41 @@ if (firstNumber > secondNumber)
 else 
 {
    Console.WriteLine($"First Number {firstNumber} is MIN, Second Number {secondNumber} is MAX");
+} */
+
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+/*Console.WriteLine ("Input the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input the third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int maxim = firstNumber;
+if (secondNumber >  firstNumber)
+{
+
+    if (thirdNumber>firstNumber)
+    {
+        maxim = thirdNumber;
+    }
+    else 
+    {
+        maxim = secondNumber;
+    }
 }
+else {
+    if ( thirdNumber>firstNumber)
+    {
+        maxim = thirdNumber;
+    }
+}
+Console.WriteLine($"Max number is {maxim}"); */
+
+
+
+
